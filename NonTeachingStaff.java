30d51d7a9a2b1eb4e80f787efd20b075dcc17ceb
Sharanya Chilari <sharanyachilari42@gmail.com>
2025-12-23 21:50:18 +0530

    Update NonTeachingStaff.java

--- NonTeachingStaff.java
+++ NonTeachingStaff.java
@@ -3,12 +3,13 @@ package com.models;
 public class NonTeachingStaff extends User {
     private String department;
 
-    public NonTeachingStaff(String username, String email, String role, String department) {
-        super(username, email, role);
+    public NonTeachingStaff(String username, String email, String password,
+            String role, String department) {
+
+super(username, email, password, role); 
         this.department = department;
     }
 
-    @Override
     public void showMenu() {
         System.out.println("Non-Teaching Staff Menu Loaded.");
     }
