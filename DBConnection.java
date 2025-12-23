a0f2b63f4e7f30c22283e716f19c01ffa79f081d
Sharanya Chilari <sharanyachilari42@gmail.com>
2025-12-23 21:55:14 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/login_system";
     private static final String USER = "root";
-    private static final String PASSWORD = "MySql@123";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         try {
