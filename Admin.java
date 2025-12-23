4396fe7e9ed6019b939cc1199b3b68a120ac3ab2
Sharanya Chilari <sharanyachilari42@gmail.com>
2025-12-23 21:46:26 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,11 +1,12 @@
 package com.models;
 
 public class Admin extends User {
-    public Admin(String username, String email, String role) {
-        super(username, email, role);
+
+    public Admin(String username, String email,
+                 String password, String role) {
+        super(username, email, password, role);
     }
 
-    @Override
     public void showMenu() {
         System.out.println("Admin Menu Loaded.");
     }
